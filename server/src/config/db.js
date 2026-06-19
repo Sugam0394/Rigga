8c2488a82161b5be09355783e9aa56b06f166c0f
Sugam0394 <singhsugam348@gmail.com>
2026-06-18 22:24:22 -0700

    start

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,34 +1,23 @@
-import mongoose from 'mongoose';
-import {DB_NAME} from '../constant.js';
+import mongoose from "mongoose";
+import { DB_NAME } from "../constant.js";
 
-<<<<<<< HEAD
 const connectDB = async () => {
   try {
-    const connectionInstance = await mongoose.connect(process.env.MONGODB_URL, {
-      dbName: DB_NAME,
-    });
+    const connectionInstance = await mongoose.connect(
+      process.env.MONGODB_URL,
+      {
+        dbName: DB_NAME,
+      }
+    );
 
     console.log(
-      `MongoB connected !! DB HOST: ${connectionInstance.connection.host} | DB NAME: ${connectionInstance.connection.name}`
+      `MongoDB connected !! DB HOST: ${connectionInstance.connection.host} | DB NAME: ${connectionInstance.connection.name}`
     );
   } catch (error) {
-    console.log('MONGODB Connection Error', error);
+    console.log("MONGODB Connection Error", error);
     process.exit(1);
   }
 };
 
 export default connectDB;
-=======
 
-const connectDB = async () => {
-  try {
-    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
-    console.log(`MongoB connected !! DB HOST: ${connectionInstance.connection.host}`)
-  } catch (error) {
-    console.log("MONGODB Connection Error", error)
-    process.exit(1)
-  }
-}
-
-export default connectDB
->>>>>>> 647a7a72d248be7ffadb6cd90eddbd7372b70035
