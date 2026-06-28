184a1e117842abd6ab0653bf903824acf800b9f9
Sugam0394 <singhsugam348@gmail.com>
2026-06-28 08:28:36 -0700

    start

--- client/src/components/LoginHero.jsx
+++ client/src/components/LoginHero.jsx
@@ -1,23 +1,10 @@
  import "./LoginHero.css";
-import riggaLogo from "../assests/RiggaLogo.png";
+ 
 
 function LoginHero() {
   return (
     <section className="login-hero">
-      <img
-        src={riggaLogo}
-        alt="Rigga Logo"
-        className="login-hero-logo"
-      />
-
-      <div className="login-hero-badge">
-       Rigga
-      </div>
-
-      <h1 className="login-hero-title">
-        Commit to what matters.
-        Prove you followed through.
-      </h1>
+    
  
  
     </section>
